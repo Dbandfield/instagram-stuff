350cff25d22e655df4470c6c70d88386495a6769
landsquid9 <daniel.bandfield@ntlworld.com>
2017-10-24 22:16:08 +0100

    media

--- index.js
+++ index.js
@@ -6,10 +6,39 @@ var https = require('https');
 port = process.env.PORT || 3000
 
 var code = 0;
+var accessToken;
+var responseJSON;
 
 var CLIENT_ID = "a2f2482c753d4f059795ff4e8d58d37c"
 var CLIENT_SECRET = "04a2f126e1e6422f97046497d6d879d3"
 var REDIRECT_URI = "https://aether-image.herokuapp.com/redirect"
+
+function requestMedia()
+{
+  var getStr = 'api.instagram.com/v1/tags/nofilter/media/recent?access_token=' + accessToken;
+
+  https.get(getStr, (resp) =>
+  {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) =>
+      {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () =>
+      {
+        console.log(JSON.parse(data).explanation);
+      });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+
+}
 
 
 function PostCode(codestring)
@@ -38,8 +67,12 @@ function PostCode(codestring)
   // Set up the request
   var post_req = https.request(post_options, function(res) {
       res.setEncoding('utf8');
-      res.on('data', function (chunk) {
+      res.on('data', function (chunk)
+      {
           console.log('Response: ' + chunk);
+          responseJSON = JSON.parse(chunk);
+          accessToken = responseJSON.access_token;
+          requestMedia();
       });
   });
 
